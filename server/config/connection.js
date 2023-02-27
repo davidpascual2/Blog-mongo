0342d39ae3bcbb4d563932993b6d67b8e3c566eb
David Pascual <davidpascual@Davids-MacBook-Pro.local>
2023-02-27 12:38:14 -0800

    fixing connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,19 @@
 const {connect, connection} = require('mongoose')
+const mongoose = require('mongoose')
 
-const connectionString =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmediaDB';
+// const connectionString =
+//     process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmediaDB';
 
-connect(connectionString, {
+// connect(connectionString, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmediaDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+ });
+
 
 
 module.exports = connection;
