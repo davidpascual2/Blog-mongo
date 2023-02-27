61b5ae91f1c23c5a0c402c4a447e68d97497342b
David Pascual <davidpascual@Davids-MacBook-Pro.local>
2023-02-27 11:38:08 -0800

    added mongodb uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const {connect, connection} = require('mongoose')
 
 const connectionString =
-    process.env.MONGODB_URI || 'mongodb+srv://davidchirp:socialmediapassword@project-cluster.8vrin1g.mongodb.net/chirpDB';
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmediaDB';
 
 connect(connectionString, {
     useNewUrlParser: true,
