49c3e822b2d2d01737acd75fb13ec9a039640947
David Pascual <davidpascual@Davids-MacBook-Pro.local>
2023-02-27 22:23:54 -0800

    using NODE_ENV

--- server/server.js
+++ server/server.js
@@ -4,10 +4,18 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 // const mongoose = require('mongoose');
 const cors = require('cors')
+const path = require('path'); //not necessary to npm i path
 const routes = require('./routes');
-
 app.use(express.json());
 app.use(cors())
+
+//=======================//
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.use(routes);
 
 //=======================//
