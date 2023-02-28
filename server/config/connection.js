42f96ac9e18edbc532775135bebd78de9d04d27b
David Pascual <davidpascual@Davids-MacBook-Pro.local>
2023-02-28 10:51:10 -0800

    removed connection options

--- server/config/connection.js
+++ server/config/connection.js
@@ -12,8 +12,6 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmediaDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
  });
 
 
